6d7bedbfb9a895a34553c785976c6afa694a4b4d
Alexandra <afortner@basecampcodingacademy.org>
2017-07-26 15:37:50 -0500

    Fixed choices in shell.py. Cleaned it up.

--- shell.py
+++ shell.py
@@ -39,10 +39,9 @@ def begin():
 
 
     first = slow_type('\n-What Audio File would you like to rent today?\n\n')
-    
 
     if first == '1':
-        scarecrow = slow_type('\nYou have chosen Dr. Crane\'s "A Study In Fear". The rental fee is 2.00. Would you like to rent it? Please input yes or no.\n\n-').title()
+        scarecrow = slow_type('\nYou have chosen Dr. Crane\'s "A Study In Fear".\nThe rental fee is 4.00, with a down payment of BLANK. Your deposit will be refunded after you return the rented Audio File.\nWould you like to rent it? Please input yes or no.\n\n-').title()
         if scarecrow == 'Yes':
             print('\nYou have paid a down of ')
         elif scarecrow == 'No':
@@ -50,15 +49,15 @@ def begin():
         else:
             print('Invalid Input.')
     elif first == '2':
-        hatter = slow_type('\nYou have chosen Jervis Tetch\'s "Wonderful Wonderland Fairy Tales!". The rental fee is 4.00. Would you like to rent it? Please input yes or no.\n\n-').title()
+        hatter = slow_type('\nYou have chosen Jervis Tetch\'s "Wonderful Wonderland Fairy Tales!".\nThe rental fee is 4.00, with a down payment of BLANK. Your deposit will be refunded after you return the rented Audio File.\nWould you like to rent it? Please input yes or no.\n\n-').title()
         if hatter == 'Yes':
-            print('\nOh! Calloo, Callay!')
+            print('\nThank you for your business! Please come again!')
         elif hatter == 'No':
             print('\nVery well! Have a nice day!')
         else:
             print('Invalid Input.')
     elif first == '3':
-        riddler = slow_type('\nYou have chosen Edward Nygma\'s "Riddles To Blow The Mind...Literally! The rental fee is 4.00. Would you like to rent it? Please input yes or no.\n\n-').title()
+        riddler = slow_type('\nYou have chosen Edward Nygma\'s "Riddles To Blow The Mind...Literally!".\nThe rental fee is 4.00, with a down payment of BLANK. Your deposit will be refunded after you return the rented Audio File.\nWould you like to rent it? Please input yes or no.\n\n-').title()
         if riddler == 'Yes':
             print('\nThank you for doing business with us!')
         elif riddler == 'No':
@@ -66,61 +65,63 @@ def begin():
         else:
             print('Invalid Input.')
     elif first == '4':
-        joker = slow_type('\nYou have chosen The Joker\'s "Ultimate Guide To Annoying The Bat". The rental fee is 4.00. Would you like to rent it? Please input yes or no.\n\n-').title()
+        joker = slow_type('\nYou have chosen The Joker\'s "Ultimate Guide To Annoying The Bat".\nThe rental fee is 4.00, with a down payment of BLANK. Your deposit will be refunded after you return the rented Audio File.\nWould you like to rent it? Please input yes or no.\n\n-').title()
         if joker == 'Yes':
-            print('\nOh! Calloo, Callay!')
+            print('\nThank you for your business! Please come again!')
         elif joker == 'No':
             print('\nVery well! Have a nice day!')
         else:
             print('Invalid Input.')
     elif first == '5':
-        harley = slow_type('\nYou have chosen Harley Quinn\'s "How To Love A Psychopath". The rental fee is 4.00. Would you like to rent it? Please input yes or no.\n\n-').title()
+        harley = slow_type('\nYou have chosen Harley Quinn\'s "How To Love A Psychopath".\nThe rental fee is 4.00, with a down payment of BLANK. Your deposit will be refunded after you return the rented Audio File.\nWould you like to rent it? Please input yes or no.\n\n-').title()
         if harley == 'Yes':
-            print('\nOh! Calloo, Callay!')
+            print('\nThank you for your business! Please come again!')
         elif harley == 'No':
             print('\nVery well! Have a nice day!')
         else:
             print('Invalid Input.')
     elif first == '6':
-        twoface = slow_type('\nYou have chosen Two-Face\'s "A Sigh Of Duality: Coping With Your Scars". The rental fee is 4.00. Would you like to rent it? Please input yes or no.\n\n-').title()
+        twoface = slow_type('\nYou have chosen Two-Face\'s "A Sigh Of Duality: Coping With Your Scars".\nThe rental fee is 4.00, with a down payment of BLANK. Your deposit will be refunded after you return the rented Audio File.\nWould you like to rent it? Please input yes or no.\n\n-').title()
         if twoface == 'Yes':
-            print('\nOh! Calloo, Callay!')
+            print('\nThank you for your business! Please come again!')
         elif twoface == 'No':
             print('\nVery well! Have a nice day!')
         else:
             print('Invalid Input.')
     elif first == '7':
-        fries = slow_type('\nYou have chosen Mr. Freeze\'s "Loving Unconditionally". The rental fee is 4.00. Would you like to rent it? Please input yes or no.\n\n-').title()
+        fries = slow_type('\nYou have chosen Mr. Freeze\'s "Loving Unconditionally".\nThe rental fee is 4.00, with a down payment of BLANK. Your deposit will be refunded after you return the rented Audio File.\nWould you like to rent it? Please input yes or no.\n\n-').title()
         if fries == 'Yes':
-            print('\nOh! Calloo, Callay!')
+            print('\nThank you for your business! Please come again!')
         elif fries == 'No':
             print('\nVery well! Have a nice day!')
         else:
             print('Invalid Input.')
     elif first == '8':
-        bane = slow_type('\nYou have chosen Bane\'s "Breaking The Bat: It\'s A Snap!". The rental fee is 4.00. Would you like to rent it? Please input yes or no.\n\n-').title()
+        bane = slow_type('\nYou have chosen Bane\'s "Breaking The Bat: It\'s A Snap!".\nThe rental fee is 4.00, with a down payment of BLANK. Your deposit will be refunded after you return the rented Audio File.\nWould you like to rent it? Please input yes or no.\n\n-').title()
         if bane == 'Yes':
-            print('\nOh! Calloo, Callay!')
+            print('\nThank you for your business! Please come again!')
         elif bane == 'No':
             print('\nVery well! Have a nice day!')
         else:
             print('Invalid Input.')
     elif first == '9':
-        strange = slow_type('\nYou have chosen Hugo Strange\'s "The Dark History Of The Medical Practice". The rental fee is 4.00. Would you like to rent it? Please input yes or no.\n\n-').title()
+        strange = slow_type('\nYou have chosen Hugo Strange\'s "The Dark History Of The Medical Practice".\nThe rental fee is 4.00, with a down payment of BLANK. Your deposit will be refunded after you return the rented Audio File.\nWould you like to rent it? Please input yes or no.\n\n-').title()
         if strange == 'Yes':
-            print('\nOh! Calloo, Callay!')
+            print('\nThank you for your business! Please come again!')
         elif strange == 'No':
             print('\nVery well! Have a nice day!')
         else:
             print('Invalid Input.')
     elif first == '10':
-        penguin = slow_type('\nYou have chosen Oswald Cobblepot\'s "Betrayal And Evil Laughter: Squawk!". The rental fee is 4.00. Would you like to rent it? Please input yes or no.\n\n-').title()
+        penguin = slow_type('\nYou have chosen Oswald Cobblepot\'s "Betrayal And Evil Laughter: Squawk!".\nThe rental fee is 4.00, with a down payment of BLANK. Your deposit will be refunded after you return the rented Audio File.\nWould you like to rent it? Please input yes or no.\n\n-').title()
         if penguin == 'Yes':
-            print('\nOh! Calloo, Callay!')
+            print('\nThank you for your business! Please come again!')
         elif penguin == 'No':
             print('\nVery well! Have a nice day!')
         else:
             print('Invalid Input.')
+    else:
+        print('Invalid Input.')
 
     
     with open('inventory.txt') as file:

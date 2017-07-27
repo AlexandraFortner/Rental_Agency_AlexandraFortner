0cd26cd5f2ed4a2d3fcc2858650d5c95a8a5b64c
Alexandra <afortner@basecampcodingacademy.org>
2017-07-27 10:06:43 -0500

    Fixed docstring in StateSalesTax Function.
    Same problem as the above commit. Fixed so there is str(price).
    
    I plan to work more on receipt, to make it more visually appealing.

--- core.py
+++ core.py
@@ -44,9 +44,16 @@ def StateSalesTax(price):
     1.2
     >>> StateSalesTax(0)
     0.0
+    >>> StateSalesTax(-15)
+    0.0
     """
+    if '-' in str(price):
+        return 0.0
+    elif '0' == str(price):
+        return 0.0
+
     state_tax = 0.08
-    return price * state_tax
+    return float(price) * state_tax
 
 def receipt(money):
     """(float) -> float
